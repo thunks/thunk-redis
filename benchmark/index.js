5c7cc53c6762edbfde7bf747c2c40116228bf51e
zensh <admin@zensh.com>
2015-04-25 11:13:49 +0800

    add ioRedis for benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,187 +1,248 @@
 'use strict';
-/*global describe, it, before, after, beforeEach, afterEach*/
-/*jshint -W106*/
 
-var Thunk = require('thunks')();
-var redis = require('../index');
-var nodeRedis = require('redis');
+const Thunk = require('thunks')();
+const redis = require('../index');
+const nodeRedis = require('redis');
+const IoRedis = require('ioredis');
 
 Thunk(function*() {
   var timeN = 0;
   var timeT = 0;
+  var timeI = 0;
   var testLen = 100000;
   var titleN = 'redis(N):';
   var titleT = 'redis(T):';
-  var clientN = nodeRedis.createClient();
-  var clientT = redis.createClient({database: 1});
+  var titleI = 'redis(I):';
+  var clientN = nodeRedis.createClient(6380);
+  var clientT = redis.createClient(6381);
+  var clientI = new IoRedis(6382);
+
+  var queue = [];
+  while (queue.length < testLen) queue.push(queue.length);
 
   var smallStr = 'teambition';
   var longStr = (new Array(4097).join('-'));
 
-  function printResult(title, timeN, timeT) {
+  function printResult(title, timeN, timeT, timeI) {
     console.log(titleN, title, Math.floor(testLen * 1000 / timeN) + ' ops/sec', '100%');
     console.log(titleT, title, Math.floor(testLen * 1000 / timeT) + ' ops/sec', ((timeN / timeT) * 100).toFixed(1) + '%');
+    console.log(titleI, title, Math.floor(testLen * 1000 / timeI) + ' ops/sec', ((timeN / timeI) * 100).toFixed(1) + '%');
     console.log('');
   }
 
-
-  console.log(titleN + 'node_redis\n', yield function(done) {
-    clientN.flushdb(done);
-  });
-  console.log(titleT + 'thunk-redis\n', yield clientT.flushdb());
-  console.log('Start...\n\n');
+  console.log(titleN + 'node_redis ', yield function(done) { clientN.flushdb(done);});
+  console.log(titleT + 'thunk-redis ', yield clientT.flushdb());
+  console.log(titleI + 'ioRedis ', yield clientI.flushdb());
+  console.log('Bench start:\n');
 
   // PING
+  yield Thunk.delay(100);
+
   timeN = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientN.ping(done);
-    });
+  yield queue.map(function() {
+    return function(done) { clientN.ping(done); };
   });
   timeN = Date.now() - timeN;
 
+  yield Thunk.delay(100);
+
   timeT = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientT.ping()(done);
-    });
+  yield queue.map(function() {
+    return clientT.ping();
   });
   timeT = Date.now() - timeT;
-  printResult('PING', timeN, timeT);
+
+  yield Thunk.delay(100);
+
+  timeI = Date.now();
+  yield queue.map(function() {
+    return clientI.ping();
+  });
+  timeI = Date.now() - timeI;
+  printResult('PING', timeN, timeT, timeI);
 
   // SET
+  yield Thunk.delay(100);
+
   timeN = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientN.set('zensh_thunks_00000001', smallStr, done);
-    });
+  yield queue.map(function() {
+    return function(done) { clientN.set('zensh_thunks_00000001', smallStr, done); };
   });
   timeN = Date.now() - timeN;
 
+  yield Thunk.delay(100);
+
   timeT = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientT.set('zensh_thunks_00000001', smallStr)(done);
-    });
+  yield queue.map(function() {
+    return clientT.set('zensh_thunks_00000001', smallStr);
   });
   timeT = Date.now() - timeT;
-  printResult('SET small string', timeN, timeT);
+
+  yield Thunk.delay(100);
+
+  timeI = Date.now();
+  yield queue.map(function() {
+    return clientI.set('zensh_thunks_00000001', smallStr);
+  });
+  timeI = Date.now() - timeI;
+  printResult('SET small string', timeN, timeT, timeI);
 
   // GET
+  yield Thunk.delay(100);
+
   timeN = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientN.get('zensh_thunks_00000001', done);
-    });
+  yield queue.map(function() {
+    return function(done) { clientN.get('zensh_thunks_00000001', done); };
   });
   timeN = Date.now() - timeN;
 
+  yield Thunk.delay(100);
+
   timeT = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientT.get('zensh_thunks_00000001')(done);
-    });
+  yield queue.map(function() {
+    return clientT.get('zensh_thunks_00000001');
   });
   timeT = Date.now() - timeT;
-  printResult('GET small string', timeN, timeT);
+
+  yield Thunk.delay(100);
+
+  timeI = Date.now();
+  yield queue.map(function() {
+    return clientI.get('zensh_thunks_00000001');
+  });
+  timeI = Date.now() - timeI;
+  printResult('GET small string', timeN, timeT, timeI);
 
   // SET
+  yield Thunk.delay(100);
+
   timeN = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientN.set('zensh_thunks_00000002', longStr, done);
-    });
+  yield queue.map(function() {
+    return function(done) { clientN.set('zensh_thunks_00000002', longStr, done); };
   });
   timeN = Date.now() - timeN;
 
+  yield Thunk.delay(100);
+
   timeT = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientT.set('zensh_thunks_00000002', longStr)(done);
-    });
+  yield queue.map(function() {
+    return clientT.set('zensh_thunks_00000002', longStr);
   });
   timeT = Date.now() - timeT;
-  printResult('SET long string', timeN, timeT);
+
+  yield Thunk.delay(100);
+
+  timeI = Date.now();
+  yield queue.map(function() {
+    return clientI.set('zensh_thunks_00000002', longStr);
+  });
+  timeI = Date.now() - timeI;
+  printResult('SET long string', timeN, timeT, timeI);
 
   // GET
+  yield Thunk.delay(100);
+
   timeN = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientN.get('zensh_thunks_00000002', done);
-    });
+  yield queue.map(function() {
+    return function(done) { clientN.get('zensh_thunks_00000002', done); };
   });
   timeN = Date.now() - timeN;
 
+  yield Thunk.delay(100);
+
   timeT = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientT.get('zensh_thunks_00000002')(done);
-    });
+  yield queue.map(function() {
+    return clientT.get('zensh_thunks_00000002');
   });
   timeT = Date.now() - timeT;
-  printResult('GET long string', timeN, timeT);
+
+  yield Thunk.delay(100);
+
+  timeI = Date.now();
+  yield queue.map(function() {
+    return clientI.get('zensh_thunks_00000002');
+  });
+  timeI = Date.now() - timeI;
+  printResult('GET long string', timeN, timeT, timeI);
 
   // INCR
+  yield Thunk.delay(100);
+
   timeN = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientN.incr('zensh_thunks_00000003', done);
-    });
+  yield queue.map(function() {
+    return function(done) { clientN.incr('zensh_thunks_00000003', done); };
   });
   timeN = Date.now() - timeN;
 
+  yield Thunk.delay(100);
+
   timeT = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientT.incr('zensh_thunks_00000003')(done);
-    });
+  yield queue.map(function() {
+    return clientT.incr('zensh_thunks_00000003');
   });
   timeT = Date.now() - timeT;
-  printResult('INCR', timeN, timeT);
+
+  yield Thunk.delay(100);
+
+  timeI = Date.now();
+  yield queue.map(function() {
+    return clientI.incr('zensh_thunks_00000003');
+  });
+  timeI = Date.now() - timeI;
+  printResult('INCR', timeN, timeT, timeI);
 
   // LPUSH
+  yield Thunk.delay(100);
+
   timeN = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientN.lpush('zensh_thunks_00000004', smallStr, done);
-    });
+  yield queue.map(function() {
+    return function(done) { clientN.lpush('zensh_thunks_00000004', smallStr, done); };
   });
   timeN = Date.now() - timeN;
 
+  yield Thunk.delay(100);
+
   timeT = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientT.lpush('zensh_thunks_00000004', smallStr)(done);
-    });
+  yield queue.map(function() {
+    return clientT.lpush('zensh_thunks_00000004', smallStr);
   });
   timeT = Date.now() - timeT;
-  printResult('LPUSH', timeN, timeT);
+
+  yield Thunk.delay(100);
+
+  timeI = Date.now();
+  yield queue.map(function() {
+    return clientI.lpush('zensh_thunks_00000004', smallStr);
+  });
+  timeI = Date.now() - timeI;
+  printResult('LPUSH', timeN, timeT, timeI);
 
   // LRANGE
+  yield Thunk.delay(100);
+
   timeN = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientN.lrange('zensh_thunks_00000004', '0', '100', done);
-    });
+  yield queue.map(function() {
+    return function(done) { clientN.lrange('zensh_thunks_00000004', '0', '100', done); };
   });
   timeN = Date.now() - timeN;
 
+  yield Thunk.delay(100);
+
   timeT = Date.now();
-  yield genTasks(testLen, function(done) {
-    setImmediate(function() {
-      clientT.lrange('zensh_thunks_00000004', '0', '100')(done);
-    });
+  yield queue.map(function() {
+    return clientT.lrange('zensh_thunks_00000004', '0', '100');
   });
   timeT = Date.now() - timeT;
-  printResult('LRANGE 100', timeN, timeT);
 
-  setTimeout(function() {
-    process.exit();
-  }, 100);
+  yield Thunk.delay(100);
+
+  timeI = Date.now();
+  yield queue.map(function() {
+    return clientI.lrange('zensh_thunks_00000004', '0', '100');
+  });
+  timeI = Date.now() - timeI;
+  printResult('LRANGE 100', timeN, timeT, timeI);
+
+  yield Thunk.delay(100);
+  process.exit();
 })();
-
-function genTasks(count, task) {
-  var tasks = [];
-  while (count--) tasks.push(task);
-  return tasks;
-}
